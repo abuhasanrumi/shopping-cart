5a442f283a8a26216642ceefb546e71182ef48a9
Abu Hasan Rumi <message.rumi@gmail.com>
2020-07-28 17:51:12 +0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let increase = document.getElementById('plusBtn');
+let itemValue = document.getElementById('itemValue');
+
+increase.addEventListener('click', function(){
+    // itemValue.value = parseInt(itemValue.value) + 1;
+    // console.log(itemValue.value)
+
+    itemValueNum = parseInt('itemValue')
+    item
+})
+
+
+
